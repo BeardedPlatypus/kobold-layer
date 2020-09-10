093745eb25a7901b9ab8e85ec05d6d7bc1963b7f
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-10 18:05:08 +0200

    AB#191: Add resource_wrapper_implementation

---
+++ kobold-layer.nucleus/src/sdl-util/resource_wrapper_implementation.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "resource_wrapper_implementation.hpp"
+
+#include <memory>
+
+#include "sdl_destructor.hpp"
+
+namespace kobold_layer::nucleus::sdl_util {
+	template <typename T>
+	resource_wrapper_implementation<T>::resource_wrapper_implementation(T* p_resource) :
+	    p_resource_(std::unique_ptr<T, sdl_destructor<T>>(p_resource)) { }
+
+	template <typename T>
+	resource_wrapper_implementation<T>::resource_wrapper_implementation(resource_wrapper_implementation&& resource_wrapper) noexcept :
+	    p_resource_(std::move(resource_wrapper.p_resource_)) { }
+
+	template <typename T>
+	resource_wrapper_implementation<T>& resource_wrapper_implementation<T>::operator=(
+		resource_wrapper_implementation&& resource_wrapper) noexcept {
+		this->p_resource_ = std::move(resource_wrapper.p_resource_);
+		return *this;
+	}
+
+	template <typename T>
+	T* resource_wrapper_implementation<T>::get_resource() const {
+		return this->p_resource_.get();
+	}
+}
---
+++ kobold-layer.nucleus/src/sdl-util/resource_wrapper_implementation.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <SDL2/SDL.h>
+#include <memory>
+
+#include "kobold-layer.nucleus/sdl-util/resource_wrapper.hpp"
+#include "sdl_destructor.hpp"
+
+namespace kobold_layer::nucleus::sdl_util {
+	/// <summary>
+	/// <see cref="resource_wrapper_implementation"/> implements the wrapper
+	/// for SDL resources such that they can be stored within smart pointers.
+	/// </summary>
+	/// <typeparam name="T">The type of the resource being wrapped. </typeparam>
+	template <typename T>
+	class resource_wrapper_implementation final : public resource_wrapper<T> {
+	public:
+		/// <summary>
+		/// Creates a new <see cref="resource_wrapper_implementation"/>.
+		/// </summary>
+		/// <param name="p_resource">A pointer to the resource to be wrapped.</param>
+		explicit resource_wrapper_implementation(T* p_resource);
+		~resource_wrapper_implementation() = default;
+
+		resource_wrapper_implementation(const resource_wrapper_implementation&) = delete;
+		resource_wrapper_implementation& operator=(const resource_wrapper_implementation&) = delete;
+
+		resource_wrapper_implementation(resource_wrapper_implementation&& resource_wrapper) noexcept;
+		resource_wrapper_implementation& operator=(resource_wrapper_implementation&& resource_wrapper) noexcept;
+
+
+		T* get_resource() const override;
+
+	private:
+		std::unique_ptr<T, sdl_destructor<T>> p_resource_;
+	};
+
+	template class resource_wrapper_implementation<SDL_Window>;
+	template class resource_wrapper_implementation<SDL_Renderer>;
+	template class resource_wrapper_implementation<SDL_Texture>;
+}
