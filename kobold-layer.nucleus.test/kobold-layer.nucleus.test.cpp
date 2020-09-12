3ba67ae7e6ba8c12b70efe2fbbbd0d8453292831
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-12 10:29:10 +0200

    AB#192: Add kobold-layer.nucleus.test

---
+++ kobold-layer.nucleus.test/kobold-layer.nucleus.test.cpp
@@ -0,0 +1,10 @@
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+	// The following line must be executed to initialise Google Mock (and
+	// Google Test) before running the tests.
+	::testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
