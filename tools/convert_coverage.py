f7aca0f84bb1ab8fb24efa1fd9709223fb2c02fc
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-15 21:05:57 +0200

    AB#195: adjust convert_coverage to copy .trx files correctly
    [skip ci]

--- tools/convert_coverage.py
+++ tools/convert_coverage.py
@@ -28,8 +28,20 @@ def find_coverage_files(src_path: Path) -> Sequence:
     """
     return Path(src_path).glob("**/*.coverage")
 
+def find_trx_files(src_path: Path) -> Sequence:
+    """
+    Find the coverage files within the specified src_path.
 
-def copy_coverage_files_to_cwd(coverage_files: Sequence) -> None:
+    Parameters:
+        src_path (Path): The path in which to look for the .coverage files.
+    
+    Returns:
+        (Sequence) The set of .coverage files within the specified folder.
+    """
+    return Path(src_path).glob("**/*.trx")
+
+
+def copy_files_to_cwd(coverage_files: Sequence, suffix: str) -> None:
     """
     Copy the Sequence of coverage files to the current working 
     directory as coverage_<n>.coverage.
@@ -38,14 +50,13 @@ def copy_coverage_files_to_cwd(coverage_files: Sequence) -> None:
         coverage_files (Sequence): Sequence of Paths to copy to the working directory
     """
     i = 0
-    new_coverage_name_template = "coverage_{}.coverage"
+    new_coverage_name_template = "coverage_{}{}"
 
     for cov_path in coverage_files:
-        new_name = new_coverage_name_template.format(i)
+        new_name = new_coverage_name_template.format(i, suffix)
         goal_path = Path(new_name)
 
         shutil.move(str(cov_path), str(goal_path))
-        shutil.move(str(cov_path.with_suffix(".trx")), str(goal_path.with_suffix(".trx")))
 
         i += 1
 
@@ -96,7 +107,10 @@ def run(coverage_dir: Path) -> None:
 
     print("##[debug] " + str(coverage_dir))
     src_coverage_files = find_coverage_files(coverage_dir)
-    copy_coverage_files_to_cwd(src_coverage_files)
+    copy_files_to_cwd(src_coverage_files, ".coverage")
+
+    src_trx_files = find_trx_files(coverage_dir)
+    copy_files_to_cwd(src_trx_files, ".trx")
 
     cwd_coverage_files = find_coverage_files(".")
 
