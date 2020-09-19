41abdcf4f8f45618fd485827b27fb9473b602433
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-19 16:07:53 +0200

    AB#191: Add texture_factory_implementation

---
+++ kobold-layer.nucleus/src/texture/texture_factory_implementation.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "texture_factory_implementation.hpp"
+#include "texture_implementation.hpp"
+
+
+namespace kobold_layer::nucleus::texture
+{
+	texture_factory_implementation::texture_factory_implementation(
+		std::shared_ptr<sdl_util::resource_wrapper<SDL_Renderer>> const p_sdl_renderer,
+		std::shared_ptr<sdl_util::sdl_dispatcher> const p_dispatcher) :
+		p_renderer_(p_sdl_renderer),
+		p_dispatcher_(p_dispatcher)
+	{
+	}
+
+	std::unique_ptr<texture> texture_factory_implementation::construct_texture(
+		std::filesystem::path const& texture_path) const
+	{
+		std::unique_ptr<sdl_util::resource_wrapper<struct SDL_Texture>> p_texture =
+			p_dispatcher_->load_texture(
+				this->p_renderer_->get_resource(),
+				texture_path.string());
+
+		return std::make_unique<texture_implementation>(std::move(p_texture),
+			this->p_dispatcher_);
+	}
+}
---
+++ kobold-layer.nucleus/src/texture/texture_factory_implementation.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <filesystem>
+
+#include "kobold-layer.nucleus/sdl-util/resource_wrapper.hpp"
+#include "kobold-layer.nucleus/sdl-util/sdl_dispatcher.hpp"
+#include "kobold-layer.nucleus/texture/texture.hpp"
+
+#include "texture_factory.hpp"
+
+
+namespace kobold_layer::nucleus::texture
+{
+	/// <summary>
+	/// <see cref="texture_factory_implementation"/> implements the interface to
+	/// construct <see cref="texture"/> objects.
+	/// </summary>
+	class texture_factory_implementation final : public texture_factory
+	{
+	public:
+		/// <summary>
+		/// Creates a new <see cref="texture_factory_implementation"/>.
+		/// </summary>
+		/// <param name="p_sdl_renderer">A pointer to the <see cref="SDL_Renderer"/>.</param>
+		/// <param name="p_dispatcher">The <see cref="sdl_dispatcher"/>.</param>
+		texture_factory_implementation(
+			std::shared_ptr<sdl_util::resource_wrapper<SDL_Renderer>> p_sdl_renderer,
+			std::shared_ptr<sdl_util::sdl_dispatcher> p_dispatcher);
+
+		[[nodiscard]] std::unique_ptr<texture> construct_texture(std::filesystem::path const& texture_path) const override;
+
+	private:
+		std::shared_ptr<sdl_util::resource_wrapper<SDL_Renderer>> p_renderer_;
+		std::shared_ptr<sdl_util::sdl_dispatcher> p_dispatcher_;
+	};
+}
