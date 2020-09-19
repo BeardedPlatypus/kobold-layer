c3a2c1d8f2513c3a83c16956fa2b0ea38ed5627d
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-19 15:53:48 +0200

    AB#191: Add texture_factory

---
+++ kobold-layer.nucleus/src/texture/texture_factory.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <filesystem>
+#include "kobold-layer.nucleus/texture/texture.hpp"
+
+namespace kobold_layer::nucleus::texture
+{
+	/// <summary>
+	/// <see cref="texture_factory_implementation"/> implements the interface to
+	/// construct <see cref="texture"/> objects.
+	/// </summary>
+	class texture_factory
+	{
+	public:
+		texture_factory(const texture_factory&) = delete;
+		texture_factory& operator=(const texture_factory&) = delete;
+
+		texture_factory(texture_factory&&) noexcept = delete;
+		texture_factory& operator=(texture_factory&&) noexcept = delete;
+
+		virtual ~texture_factory() = default;
+
+		/// <summary>
+		/// Constructs the texture from the given <paramref name="texture_path"/>.
+		/// </summary>
+		/// <param name="texture_path">The path to the texture.</param>
+		/// <returns>
+		/// A new <see cref="std::unique_ptr<{texture}"/>.
+		/// </returns>
+		[[nodiscard]] virtual std::unique_ptr<texture> construct_texture(const std::filesystem::path& texture_path) const = 0;
+
+	protected:
+		texture_factory() = default;
+	};
+}
