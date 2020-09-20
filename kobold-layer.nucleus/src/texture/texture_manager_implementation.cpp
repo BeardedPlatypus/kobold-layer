a6c71acde16c660234db6fa177ba6f9383b9323d
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-20 11:03:48 +0200

    AB#191: Add texture_manager_implementation

---
+++ kobold-layer.nucleus/src/texture/texture_manager_implementation.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "texture_manager_implementation.hpp"
+
+namespace kobold_layer::nucleus::texture
+{
+	texture_manager_implementation::texture_manager_implementation(std::unique_ptr<texture_factory> p_texture_factory) :
+		p_texture_factory_(std::move(p_texture_factory))
+	{
+	}
+
+	void texture_manager_implementation::load_texture(std::string const& key, std::filesystem::path const& texture_path)
+	{
+		if (this->has_texture(key)) return;
+
+		this->texture_map_.emplace(
+			key,
+			this->p_texture_factory_->construct_texture(texture_path.string()));
+	}
+
+	bool texture_manager_implementation::has_texture(std::string const& key) const
+	{
+		return this->texture_map_.find(key) != this->texture_map_.end();
+	}
+
+	const texture* texture_manager_implementation::get_texture(std::string const& key) const
+	{
+		return this->texture_map_.at(key).get();
+	}
+}
---
+++ kobold-layer.nucleus/src/texture/texture_manager_implementation.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <unordered_map>
+
+#include "kobold-layer.nucleus/texture/texture.hpp"
+
+#include "texture_factory.hpp"
+#include "texture_manager.hpp"
+
+namespace kobold_layer::nucleus::texture
+{
+	/// <summary>
+	/// <see cref="texture_manager_implementation"/> implements the
+	/// interface to create and store <see cref="texture"/> objects.
+	/// </summary>
+	class texture_manager_implementation final : public texture_manager
+	{
+	public:
+		/// <summary>
+		/// Creates a new <see cref="texture_manager_implementation"/>.
+		/// </summary>
+		/// <param name="p_texture_factory">
+		/// A pointer to the <see cref="texture_factory"/> to create the textures with.
+		/// </param>
+		explicit texture_manager_implementation(std::unique_ptr<texture_factory> p_texture_factory);
+
+		void load_texture(std::string const& key,
+			              std::filesystem::path const& texture_path) override;
+
+		[[nodiscard]] bool has_texture(std::string const& key) const override;
+		[[nodiscard]] const texture* get_texture(std::string const& key) const override;
+
+	private:
+		using texture_map = std::unordered_map<std::string, std::unique_ptr<texture>>;
+		texture_map texture_map_ = texture_map();
+
+		std::unique_ptr<texture_factory> p_texture_factory_;
+	};
+}
