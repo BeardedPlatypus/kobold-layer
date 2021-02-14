6341e46152ea10c1f3b3a2eca9d4ff1d2f5d9f50
BeardedPlatypus <mwtegelaers@gmail.com>
2021-02-14 12:27:51 +0100

    AB#223: Add canvas_implementation

---
+++ kobold-layer.nucleus/src/render/canvas_implementation.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "canvas_implementation.hpp"
+
+namespace kobold_layer::nucleus::render
+{
+	canvas_implementation::canvas_implementation(std::shared_ptr<renderer> p_renderer, 
+		                                         std::unique_ptr<viewport> p_viewport) :
+	    p_renderer_(p_renderer),
+	    p_viewport_(std::move(p_viewport)) { }
+
+	void canvas_implementation::render_copy(SDL_Texture* p_texture, 
+		                                    rectangle<int> const& texture_clip, 
+		                                    rectangle<float> const& destination, 
+		                                    float const angle, 
+		                                    bool const flip_horizontally,
+		                                    bool const flip_vertically) const
+	{
+		if (auto clipped_rects = this->p_viewport_->clip_to_viewport(texture_clip, destination))
+		{
+			this->p_renderer_->render_copy(
+				p_texture,
+				clipped_rects->source,
+				clipped_rects->target,
+				angle, 
+				flip_horizontally, 
+				flip_vertically);
+		}
+	}
+
+	void canvas_implementation::present() const
+	{
+		this->p_renderer_->render_present();
+	}
+
+	void canvas_implementation::clear() const
+	{
+		this->p_renderer_->render_clear();
+	}
+}
+
+	
---
+++ kobold-layer.nucleus/src/render/canvas_implementation.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <memory>
+
+#include "kobold-layer.nucleus/render/canvas.hpp"
+#include "kobold-layer.nucleus/render/renderer.hpp"
+#include "kobold-layer.nucleus/render/viewport.hpp"
+
+namespace kobold_layer::nucleus::render
+{
+    /// <summary>
+    /// <see cref="canvas_implementation"/> implements the interface with which to render
+    /// a <see cref="SDL_Texture"/> to world space coordinates.
+    /// </summary>
+	class canvas_implementation final : public canvas
+	{
+	public:
+		/// <summary>
+		/// Construct a new <see cref="canvas_implementation"/> with the given
+		/// pointer to the <see cref="renderer"/> and <see cref="viewport"/>.
+		/// </summary>
+		/// <param name="p_renderer">Pointer to the renderer to use.</param>
+		/// <param name="p_viewport">Pointer to the viewport to use.</param>
+		canvas_implementation(std::shared_ptr<renderer> p_renderer,
+		                      std::unique_ptr<viewport> p_viewport);
+
+		void render_copy(SDL_Texture* p_texture, 
+			             rectangle<int> const& texture_clip,
+			             rectangle<float> const& destination, 
+			             float angle, 
+			             bool flip_horizontally,
+			             bool flip_vertically) const override;
+		void present() const override;
+		void clear() const override;
+	
+	private:
+		std::shared_ptr<renderer> const p_renderer_;
+		std::unique_ptr<viewport> const p_viewport_;
+	};
+	
+}
