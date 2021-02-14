442ff9a347d93d30dfdd7cee7e35ad3bd0f510aa
BeardedPlatypus <mwtegelaers@gmail.com>
2021-02-14 22:27:25 +0100

    AB#223: Add initial viewport_implementation

---
+++ kobold-layer.nucleus/src/render/viewport_implementation.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "viewport_implementation.hpp"
+
+namespace kobold_layer::nucleus::render
+{
+	viewport_implementation::viewport_implementation(std::shared_ptr<world> p_world,
+		                                             unsigned int const render_width,
+		                                             unsigned int const render_height,
+	                                                 rectangle<float> const& viewport_coordinates) :
+	    p_world_(std::move(p_world)),
+	    p_viewport_coordinates_(std::make_unique<rectangle<float>>(viewport_coordinates.x, 
+			                                                       viewport_coordinates.y,
+			                                                       viewport_coordinates.width,
+			                                                       viewport_coordinates.height)),
+	    render_width_(render_width),
+	    render_height_(render_height) { }
+
+	unsigned int viewport_implementation::get_render_width() const
+	{
+		return this->render_width_;
+	}
+
+	void viewport_implementation::set_render_width(unsigned int const new_render_width)
+	{
+		this->render_width_ = new_render_width;
+	}
+
+	unsigned int viewport_implementation::get_render_height() const
+	{
+		return this->render_height_;
+	}
+
+	void viewport_implementation::set_render_height(unsigned int const new_render_height)
+	{
+		this->render_height_ = new_render_height;
+	}
+
+	
+	rectangle<float> viewport_implementation::get_viewport_coordinates() const
+	{
+		return *this->p_viewport_coordinates_;
+	}
+	void viewport_implementation::set_viewport_coordinates(rectangle<float> const& new_coordinates)
+	{
+		this->p_viewport_coordinates_ = 
+			std::make_unique<rectangle<float>>(
+				new_coordinates.x,
+				new_coordinates.y,
+				new_coordinates.width,
+				new_coordinates.height);
+	}
+
+	std::optional<viewport::clipped_rects> viewport_implementation::clip_to_viewport(
+			rectangle<int> const& tex_source,
+			rectangle<float> const& world_destination) const
+	{
+		// TODO: implement this behaviour correctly.
+		return {};
+	}
+}
---
+++ kobold-layer.nucleus/src/render/viewport_implementation.hpp
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include "kobold-layer.nucleus/render/viewport.hpp"
+
+#include <memory>
+
+#include "kobold-layer.nucleus/render/world.hpp"
+
+namespace kobold_layer::nucleus::render
+{
+	/// <summary>
+	/// <see cref="viewport_implementation" /> implements the <see cref="viewport" /> interface.
+	/// </summary>
+	/// <seealso cref="viewport" />
+	class viewport_implementation : public viewport
+	{
+	public:		
+        /// <summary>
+        /// Creates a new <see cref="viewport_implementation"/>.
+        /// </summary>
+        /// <param name="p_world">A pointer to the world.</param>
+        /// <param name="render_width">Width of the render.</param>
+        /// <param name="render_height">Height of the render.</param>
+        /// <param name="viewport_coordinates">The viewport coordinates.</param>
+		viewport_implementation(
+			std::shared_ptr<world> p_world,
+			unsigned int render_width,
+			unsigned int render_height,
+			rectangle<float> const& viewport_coordinates);
+		
+		[[nodiscard]] unsigned int get_render_width() const override;
+		void set_render_width(unsigned int new_render_width) override;
+
+		[[nodiscard]] unsigned int get_render_height() const override;
+		void set_render_height(unsigned int new_render_height) override;
+
+		[[nodiscard]] rectangle<float> get_viewport_coordinates() const override;
+		void set_viewport_coordinates(rectangle<float> const& new_coordinates) override;
+
+		[[nodiscard]] std::optional<viewport::clipped_rects> clip_to_viewport(
+			rectangle<int> const& tex_source,
+			rectangle<float> const& world_destination) const override;
+
+	private:
+		std::shared_ptr<world> p_world_;
+
+		std::unique_ptr<rectangle<float>> p_viewport_coordinates_;
+
+		unsigned int render_width_;
+		unsigned int render_height_;
+	};
+}
