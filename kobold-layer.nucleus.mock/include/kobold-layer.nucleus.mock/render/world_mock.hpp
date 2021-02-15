73ebf2a2084634f48950f61bf6737ebc7e842a41
BeardedPlatypus <mwtegelaers@gmail.com>
2021-02-15 14:51:49 +0100

    AB#223: Add world_mock

---
+++ kobold-layer.nucleus.mock/include/kobold-layer.nucleus.mock/render/world_mock.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "kobold-layer.nucleus/render/world.hpp"
+#include <gmock/gmock.h>
+
+class world_mock final : public kobold_layer::nucleus::render::world
+{
+public:
+	MOCK_METHOD(kobold_layer::nucleus::render::rectangle<float>, get_boundaries, (), (const));
+	MOCK_METHOD(void, set_boundaries, (kobold_layer::nucleus::render::rectangle<float> const&), ());
+};
