72907dc1d5100e9fd5d0ef48964f62794c5a58f8
BeardedPlatypus <mwtegelaers@gmail.com>
2021-02-16 22:25:57 +0100

    AB#223: Add canvas_mock

---
+++ kobold-layer.nucleus.mock/include/kobold-layer.nucleus.mock/render/canvas_mock.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "kobold-layer.nucleus/render/canvas.hpp"
+#include <gmock/gmock.h>
+
+class canvas_mock final : public kobold_layer::nucleus::render::canvas
+{
+public:
+	MOCK_METHOD(void, render_copy,
+		(SDL_Texture*,
+         kobold_layer::nucleus::render::rectangle<int> const&,
+         kobold_layer::nucleus::render::rectangle<float> const&,
+         float, bool, bool),
+		(const));
+
+	MOCK_METHOD(void, render_present, (), (const));
+	MOCK_METHOD(void, render_clear, (), (const));
+};
